12a95ddf922a0e5e836a7eee2bbfdd8a1e7347ad
Rz116 <147172188+Rz116@users.noreply.github.com>
2024-09-23 17:18:47 -0400

    Update arithmetic.js

--- arithmetic.js
+++ arithmetic.js
@@ -1,34 +1,114 @@
-window.addEventListener("load",addlistener);
-var Num1, Num2, Result;
+window.addEventListener("load",Buttons)
+var Num1, Num2, Answer 
 
-function addlistener()
+function Buttons()
 {
-	document.getElementById("BtnAdd").addEventListener("click",Addition);
+	document.getElementById("BtnAdd").addEventListener("click",addition);
+	document.getElementById("BtnSub").addEventListener("click",Subtract);
+	document.getElementById("BtnDiv").addEventListener("click",Divide);
+	document.getElementById("BtnMul").addEventListener("click",Multiply);
 }
-function Addition()
+
+function addition()
 {
-	Num1 = document.getElementById("intnum1").value;
-	Num2 = document.getElementById("intnum2").value;
-	if(Firstname == " " || Lastname == " " )
+	Num1 = document.getElementById("IntNum1").value;
+	if(Num1 == "" )
 		{
-			alert("You didnt type a input in one of the boxes");
-			autofocus();
+			alert("Type in a input")
+			Autofocus();
 		}
 	else
 		{
-			Result = Number(Num1) + Number(Num2);
-			document.getElementById("lblmessage").textContent = "Result" + Result;
+			Num2 = document.getElementById("IntNum2").value;
+			if(Num2 == "")
+			{
+				alert("Type in a input");
+				Autofocus()
+			}
+			else
+			{
+				Answer = Number(Num1) + Number(Num2);
+				document.getElementById("pmessage").innerHTML = "Answer: " + Answer;
+			}
 		}
 }
-
-function autofocus()
+function Subtract()
 {
-	if(Firstname == " ")
+	Num1 = document.getElementById("IntNum1").value;
+	if(Num1 == "" )
 		{
-			document.getElementById("intnum1").focus();
+			alert("Type in a input")
+			Autofocus();
 		}
 	else
 		{
-			document.getElementById("intnum2").focus();
+			Num2 = document.getElementById("IntNum2").value;
+			if(Num2 == "")
+			{
+				alert("Type in a input");
+				Autofocus()
+			}
+			else
+			{
+				Answer = Number(Num1) - Number(Num2);
+				document.getElementById("pmessage").innerHTML = "Answer: " + Answer;
+			}
 		}
+}
+function Divide()
+{
+	Num1 = document.getElementById("IntNum1").value;
+	if(Num1 == "" )
+		{
+			alert("Type in a input")
+			Autofocus();
+		}
+	else
+		{
+			Num2 = document.getElementById("IntNum2").value;
+			if(Num2 == "")
+			{
+				alert("Type in a input");
+				Autofocus()
+			}
+			else
+			{
+				Answer = Number(Num1) / Number(Num2);
+				document.getElementById("pmessage").innerHTML = "Answer: " + Answer;
+			}
+		}
+}
+function Multiply()
+{
+	Num1 = document.getElementById("IntNum1").value;
+	if(Num1 == "" )
+		{
+			alert("Type in a input")
+			Autofocus();
+		}
+	else
+		{
+			Num2 = document.getElementById("IntNum2").value;
+			if(Num2 == "")
+			{
+				alert("Type in a input");
+				Autofocus()
+			}
+			else
+			{
+				Answer = Number(Num1) * Number(Num2);
+				document.getElementById("pmessage").innerHTML = "Answer: " + Answer;
+			}
+		}
+}
+function Autofocus()
+{
+	if (Num1 == "")
+	{
+		document.getElementById("IntNum1").focus();
+	}
+	else
+	{
+		document.getElementById("IntNum2").focus();
+	}
 }
