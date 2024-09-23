85182874db31685b597886d4b287de0a6329cd33
Rz116 <147172188+Rz116@users.noreply.github.com>
2024-09-23 10:15:08 -0400

    Update arithmetic.js

--- arithmetic.js
+++ arithmetic.js
@@ -1,8 +1,34 @@
-/**
- * 
- */
-function PerformCalc()
+window.addEventListener("load",addlistener);
+var Num1, Num2, Result;
+
+function addlistener()
 {
-	return(2+3);
+	document.getElementById("BtnAdd").addEventListener("click",Addition);
 }
-document.write("The sum of 2+3 = " + PerformCalc());
+function Addition()
+{
+	Num1 = document.getElementById("intnum1").value;
+	Num2 = document.getElementById("intnum2").value;
+	if(Firstname == " " || Lastname == " " )
+		{
+			alert("You didnt type a input in one of the boxes");
+			autofocus();
+		}
+	else
+		{
+			Result = Number(Num1) + Number(Num2);
+			document.getElementById("lblmessage").textContent = "Result" + Result;
+		}
+}
+
+function autofocus()
+{
+	if(Firstname == " ")
+		{
+			document.getElementById("intnum1").focus();
+		}
+	else
+		{
+			document.getElementById("intnum2").focus();
+		}
+}
