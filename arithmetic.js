449c296410dad2ccaa0d2ffd220bcbbfa23f398e
Rz116 <147172188+Rz116@users.noreply.github.com>
2024-09-29 21:54:33 -0400

    Update arithmetic.js

--- arithmetic.js
+++ arithmetic.js
@@ -1,114 +1,20 @@
-window.addEventListener("load",Buttons)
-var Num1, Num2, Answer 
+window.addEventListener("load",buttons);
+var num1, num2, result;
 
-function Buttons()
+function buttons()
 {
-	document.getElementById("BtnAdd").addEventListener("click",addition);
-	document.getElementById("BtnSub").addEventListener("click",Subtract);
-	document.getElementById("BtnDiv").addEventListener("click",Divide);
-	document.getElementById("BtnMul").addEventListener("click",Multiply);
+	document.getElementById("Biginput").disabled = true;
+	document.getElementById("one").addEventListener("click",one);
+	document.getElementById("Clear").addEventListener("click",clear);
 }
-
-function addition()
+function one()
 {
-	Num1 = document.getElementById("IntNum1").value;
-	if(Num1 == "" )
-		{
-			alert("Type in a input")
-			Autofocus();
-		}
-	else
-		{
-			Num2 = document.getElementById("IntNum2").value;
-			if(Num2 == "")
-			{
-				alert("Type in a input");
-				Autofocus()
-			}
-			else
-			{
-				Answer = Number(Num1) + Number(Num2);
-				document.getElementById("pmessage").innerHTML = "Answer: " + Answer;
-			}
-		}
+		num1 = 1;
+		document.getElementById("Biginput").value = num1;
+		document.getElementById("Biginput").disabled = true;
 }
-function Subtract()
+function clear()
 {
-	Num1 = document.getElementById("IntNum1").value;
-	if(Num1 == "" )
-		{
-			alert("Type in a input")
-			Autofocus();
-		}
-	else
-		{
-			Num2 = document.getElementById("IntNum2").value;
-			if(Num2 == "")
-			{
-				alert("Type in a input");
-				Autofocus()
-			}
-			else
-			{
-				Answer = Number(Num1) - Number(Num2);
-				document.getElementById("pmessage").innerHTML = "Answer: " + Answer;
-			}
-		}
-}
-function Divide()
-{
-	Num1 = document.getElementById("IntNum1").value;
-	if(Num1 == "" )
-		{
-			alert("Type in a input")
-			Autofocus();
-		}
-	else
-		{
-			Num2 = document.getElementById("IntNum2").value;
-			if(Num2 == "")
-			{
-				alert("Type in a input");
-				Autofocus()
-			}
-			else
-			{
-				Answer = Number(Num1) / Number(Num2);
-				document.getElementById("pmessage").innerHTML = "Answer: " + Answer;
-			}
-		}
-}
-function Multiply()
-{
-	Num1 = document.getElementById("IntNum1").value;
-	if(Num1 == "" )
-		{
-			alert("Type in a input")
-			Autofocus();
-		}
-	else
-		{
-			Num2 = document.getElementById("IntNum2").value;
-			if(Num2 == "")
-			{
-				alert("Type in a input");
-				Autofocus()
-			}
-			else
-			{
-				Answer = Number(Num1) * Number(Num2);
-				document.getElementById("pmessage").innerHTML = "Answer: " + Answer;
-			}
-		}
-}
-function Autofocus()
-{
-	if (Num1 == "")
-	{
-		document.getElementById("IntNum1").focus();
-	}
-	else
-	{
-		document.getElementById("IntNum2").focus();
-	}
+	num1 = document.getElementById("Biginput").value = "";
+	document.getElementById("Biginput").disabled = true;
 }
