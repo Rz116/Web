413762a43718387d8dfc09ef0ca3a035d88d8f27
Rz116 <147172188+Rz116@users.noreply.github.com>
2024-10-19 14:38:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,17 +11,37 @@ function Number()
 	Grade_number = document.getElementById("txtGrades").value;
 	if (Grade_number == "")
 		{
-			alert("TYpe in a number");
+			alert("Type in a number");
 		}
 	else
 		{
 			document.getElementById("LblNumberGrades").textContent = "Grades: ";
 			document.getElementById("txtGrades").value = "";
-			Grade(Grade_number)
+			Function()
 		}
 }
-
-function Grade(Amount_Grade)
-{                          
+function Function()
+{
 	document.getElementById("btnSubmit").addEventListener("click",Grade);
+}
+function Grade()
+{
+	var sum = 0
+	for (let i; i < Grade_number; i++)
+	{
+		var grade = parseFloat(document.getElementById("txtGrades").value);
+		document.getElementById("txtGrades").value = "";
+		if(grade > 100 && grade < 0)
+		{
+			alert("Type in a correct input")
+			document.getElementById("txtGrades").focus(); 
+			Grade()
+		}
+		else
+		{
+			document.getElementById("txtGrades").value = "";
+			sum = sum + grade; 
+		}
+	}
+	document.getElementById("lblmessage").textContent = sum;
 }
