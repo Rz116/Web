c860df54beafef97929484899e4e6aa48b8f779f
Rz116 <147172188+Rz116@users.noreply.github.com>
2024-12-19 17:14:35 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,1 +1,29 @@
-
+window.addEventListener("load",direct); 
+function direct()
+{
+    document.getElementById("benefit1").addEventListener("click",Direct1);
+    document.getElementById("benefit2").addEventListener("click",Direct2);
+    document.getElementById("consequence1").addEventListener("click",Direct4);
+    document.getElementById("consequence2").addEventListener("click",Direct5);
+    document.getElementById("BM").addEventListener("click",Direct3);
+}
+function Direct1()
+{
+    window.location.href = "benefit1.html";
+}
+function Direct2()
+{
+    window.location.href = "benefit2.html";
+}
+function Direct3()
+{
+    window.location.href = "Home.html"
+}
+function Direct4()
+{
+    window.location.href = "consequence1.html";
+}
+function Direct5()
+{
+    window.location.href = "consequence2.html";
+}
