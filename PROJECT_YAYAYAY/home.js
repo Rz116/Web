35d1c94a7b73e5f8947e6dc6a381a33119f8118f
Rz116 <147172188+Rz116@users.noreply.github.com>
2024-12-29 17:46:00 -0500

    Add files via upload

---
+++ PROJECT_YAYAYAY/home.js
@@ -0,0 +1,34 @@
+window.addEventListener("load",direct); 
+function direct()
+{
+    document.getElementById("benefit1").addEventListener("click",Direct1);
+    document.getElementById("benefit2").addEventListener("click",Direct2);
+    document.getElementById("consequence1").addEventListener("click",Direct4);
+    document.getElementById("consequence2").addEventListener("click",Direct5);
+    document.getElementById("BM").addEventListener("click",Direct3);
+    document.getElementById("Weight_Tracker").addEventListener("click",Direct6);
+}
+function Direct1()
+{
+    window.location.href = "benefit1.html";
+}
+function Direct2()
+{
+    window.location.href = "benefit1.html";
+}
+function Direct3()
+{
+    window.location.href = "Home.html"
+}
+function Direct4()
+{
+    window.location.href = "consequence1.html";
+}
+function Direct5()
+{
+    window.location.href = "consequence1.html";
+}
+function Direct6()
+{
+    window.location.href = "tracker.html";
+}
---
+++ PROJECT_YAYAYAY/tracker.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",button); 
+function button()
+{
+    document.getElementById("Home").addEventListener("click",Home_Page);
+}
+function Home_Page()
+{
+    window.location.href = "Home.html";
+}
