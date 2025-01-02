1eee0bdae955fba43fa08b7898217ba116cef023
Rz116 <147172188+Rz116@users.noreply.github.com>
2025-01-01 22:14:05 -0500

    Delete PROJECT_YAYAYAY directory

--- PROJECT_YAYAYAY/information.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load",home);
-function home()
-{
-    document.getElementById("Heading").addEventListener("click",gohome);
-}
-function gohome()
-{
-    window.location.href = "Home.html";
-}
--- PROJECT_YAYAYAY/tracker.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load",button); 
-function button()
-{
-    document.getElementById("Home").addEventListener("click",Home_Page);
-}
-function Home_Page()
-{
-    window.location.href = "Home.html";
-}
