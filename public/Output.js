9e476c7a2b853b872d066fe67b1172af99f42327
Rz116 <147172188+Rz116@users.noreply.github.com>
2025-01-07 10:02:28 -0500

    Add files via upload

---
+++ public/Output.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",events); 
+function events()
+{
+	document.getElementById("Home").addEventListener("click",home);
+}
+function home()
+{
+	window.location.href = "Home.html";
+}
