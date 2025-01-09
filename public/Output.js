0ca5ea819ace499130fb25b8184e890bdb39d704
Rz116 <147172188+Rz116@users.noreply.github.com>
2025-01-08 22:19:48 -0500

    Delete public directory

--- public/Output.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load",events); 
-function events()
-{
-	document.getElementById("Home").addEventListener("click",home);
-}
-function home()
-{
-	window.location.href = "Home.html";
-}
