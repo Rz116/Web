22c6484b52eab9ec283e71c580c9a972955469e5
Rz116 <147172188+Rz116@users.noreply.github.com>
2025-01-15 21:32:12 -0500

    Add files via upload

---
+++ PROJECT_YAYAYAY/information.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",home);
+function home()
+{
+    document.getElementById("Heading").addEventListener("click",gohome);
+}
+function gohome()
+{
+    window.location.href = "Home.html";
+}
