78f8b51e046b67ca3c5b6bb9fef0db8f77c1159d
Rz116 <147172188+Rz116@users.noreply.github.com>
2025-03-20 10:09:41 -0400

    Update tutorial_6.py

--- tutorial_6.py
+++ tutorial_6.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 @app.route('/', methods = ["GET","POST"])
-
 def main():
     if(request.method == "GET"):
         return render_template("index1.html")
@@ -13,9 +12,16 @@ def main():
 @app.route('/info', methods = ["GET","POST"])
 def mainscreen():    
     global headings, data
-    period = request.form.get("periodsinput")
+    if (request.method == "POST"):           
+        period = request.form.get("periodsinput") 
+        return render_template('index2.html', periodnumber = int(period))
+    else:
+        outputscreen()
+        return render_template("index3.html")
     
-    return render_template('index2.html', periodnumber = int(period))
+@app.route('/output',methods = ["GET","POST"])
+def outputscreeen():
+    return render_template("index3.html")
     
 if __name__ == "__main__":
     app.run()
