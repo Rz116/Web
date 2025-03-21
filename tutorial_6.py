6b0fbd4da755178f7937d4a45749d9012785bf42
Rz116 <147172188+Rz116@users.noreply.github.com>
2025-03-20 22:37:05 -0400

    Update tutorial_6.py

--- tutorial_6.py
+++ tutorial_6.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-
 app = Flask(__name__)
 @app.route('/', methods = ["GET","POST"])
 def main():
@@ -11,17 +10,49 @@ def main():
     
 @app.route('/info', methods = ["GET","POST"])
 def mainscreen():    
-    global headings, data
+    global headings, data, period
     if (request.method == "POST"):           
-        period = request.form.get("periodsinput") 
-        return render_template('index2.html', periodnumber = int(period))
+        period = request.form.get("periodsinput")
+        if (period == ""):            
+            return render_template("index1.html", periodsinput = "" )
+        else:
+            check = list(period)
+            length = len(check)
+            for i in range(0,int(length)):
+                check2 = ord(check[i])
+                if(check2 < 48 or check2 > 57):
+                    return render_template("index1.html", periodsinput = "")
+            return render_template('index2.html', periodnumber = int(period))
     else:
         outputscreen()
         return render_template("index3.html")
     
 @app.route('/output',methods = ["GET","POST"])
 def outputscreeen():
-    return render_template("index3.html")
-    
+    classes = []
+    teachers = []
+    rooms = []
+    for i in range(0,int(period)):
+        course = request.form.get("course" + str(i + 1))
+        teacher = request.form.get("teacher" + str(i+1))
+        if (len(course) < 1 or len(teacher) < 1):
+            return render_template('index2.html', periodnumber = int(period))
+        classes.append(course)
+        teachers.append(teacher)
+    for i in range(0,int(period)):
+        room = request.form.get("room" + str(i+1))
+        if(len(room) < 3):
+            return render_template('index2.html',periodnumber = int(period))
+        else:
+            char = list(room)
+            length = len(char)
+            for i in range(0,length):
+                check = ord(char[i])
+                if(check < 48 or check > 57):
+                    return render_template('index2.html',periodnumber = int(period))
+            rooms.append(room)
+    return render_template('index3.html', course = classes, teachers = teachers, rooms = rooms)
+
+        
 if __name__ == "__main__":
     app.run()
