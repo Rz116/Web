0cb7cdb8f6689da8c4a5bee33e9fe1e39f1b157f
Rz116 <147172188+Rz116@users.noreply.github.com>
2025-03-25 00:04:08 -0400

    Update tutorial_6.py

--- tutorial_6.py
+++ tutorial_6.py
@@ -10,20 +10,28 @@ def main():
     
 @app.route('/info', methods = ["GET","POST"])
 def mainscreen():    
-    global headings, data, period
+    global period, osis, grade, name
     if (request.method == "POST"):           
         period = request.form.get("periodsinput")
-        if (period == ""):            
-            return render_template("index1.html", periodsinput = "", message = "You must type something in the input box")
+        osis = request.form.get('osisinput')
+        grade = request.form.get('gradelevel')
+        name = request.form.get('fullname')
+        if (period == "" or osis == "" or len(osis) > 9 or len(osis) < 9 or grade == "" or name == ""):            
+            return render_template("index1.html", periodsinput = "", message = "You must type something in the input box and the osis must be 9 digits")
         else:
             check = list(period)
+            checkgrade = list(grade)
+            checkosis = list(osis)
             length = len(check)
             for i in range(0,int(length)):
                 check2 = ord(check[i])
-                if(check2 < 48 or check2 > 57):
-                    return render_template("index1.html", periodsinput = "", message = "You must type in a correct input(Must be number)")
+                checkgrade2 = ord(checkgrade[i])
+                checkosis2 = ord(checkosis[i])
+                print(checkgrade2,checkosis2)
+                if(int(check2) < 48 or int(check2) > 57 or int(checkgrade2) < 48 or int(checkgrade2) > 57 or int(checkosis2) < 48 or int(checkosis2) > 57):
+                    return render_template("index1.html", periodsinput = "", message = "You must type in a correct input(Period, Grade and Osis must all be numbers!)")
             if(int(period) > 9):
-                return render_template("index1.html",periodsinput  = "", message = "You must type in a correct input(It cannot be greater than 9!!)")
+                return render_template("index1.html",periodsinput  = "", message = "You must type in a correct input(Period cannot be Greater than 9)")
             else:               
                 return render_template('index2.html', periodnumber = int(period))
     else:
@@ -57,8 +65,7 @@ def outputscreeen():
             rooms.append(room)
     
     length = len(rooms)
-    return render_template('index3.html', course = classes, teachers = teachers, rooms = rooms,length = length,headings = headings)
-
+    return render_template('index3.html', course = classes, teachers = teachers, rooms = rooms,length = length,headings = headings, osis = osis, grade = grade, name = name)
         
 if __name__ == "__main__":
     app.run()
